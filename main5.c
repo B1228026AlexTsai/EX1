7b797ea57827231ee3c27b004268a106e6fd37cf
Alex Tsai <alex93.tsai@gmail.com>
2025-03-10 18:25:36 +0800

    Update main5.c

--- main5.c
+++ main5.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+void print_sp(int i, int n){
+    for (int j = 0; j < n - i; j++){
+        printf(" ");
+    }
+}
+
+void print_num(int n){
+    for (int i = 0; i < n; i++){
+        printf("%d",n);
+        printf(" ");
+    }
+}
+
+int main()
+{
+    int rows = 6;
+    for (int i = 0; i <= rows; i++){
+        print_sp(i,rows);
+        print_num(i);
+        printf("\n");
+    }
+
+    return 0;
+}
